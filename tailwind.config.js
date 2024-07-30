3d23fbf8a9aeffdf3becd01cdfcdf81eb5c6dc37
ziyadoodle <ziyadzidane69@gmail.com>
2024-07-30 10:16:49 +0700

    adding daisyui library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/Views/**/*.php"],
+  content: [
+    "./app/Views/**/*.php",
+    "./vendor/myth/auth/src/Views/**/*.php"
+  ],
+  daisyui: {
+    themes: ["light"]
+  },
   theme: {
-    fontFamily: {
-      'sans': ['"Quicksand"', 'sans-serif', 'ui-sans-serif', 'system-ui'],
-      'serif': ['ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-    },
     extend: {
+      fontFamily: {
+        'sans': ['"Quicksand"', 'Helvetica', 'Arial', 'sans-serif', 'ui-sans-serif', 'system-ui'],
+        'serif': ['ui-sans-serif', 'system-ui', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      },
       backgroundImage: {
         'hero': "url('/images/hero-1.png')",
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
